24567f548394ffc93ee06e7a5b174bb9beb656b4
Michael "Gilli" Gilliland <mjg.py3@gmail.com>
2020-04-04 08:27:36 -0400

    Add `turtle` to overall `aoc` executable.

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,13 @@
 module Main (main) where
 
-import qualified Aoc as Aoc
+import qualified Aoc
 import qualified System.Environment as Env
 
 main :: IO ()
 main = do
   arg <- Env.getArgs
   case arg of
-    [inputFile] -> do
+    [inputFile] ->
       () <$ Aoc.solve inputFile
     [inputFile, "--more-info"] -> do
       out <- Aoc.solve inputFile
@@ -20,6 +20,9 @@ main = do
           putStrLn $ "(inputType, outputType, answer, ast) = " ++ show (it, ot, answer, ast)
         Just (it, ot, answer, Aoc.Program ast) -> do
           putStrLn "Program domain:"
+          putStrLn $ "(inputType, outputType, answer, ast) = " ++ show (it, ot, answer, ast)
+        Just (it, ot, answer, Aoc.Turtle ast) -> do
+          putStrLn "Turtle domain:"
           putStrLn $ "(inputType, outputType, answer, ast) = " ++ show (it, ot, answer, ast)
         Nothing -> pure ()
     _       -> print "One argument (file name) expected"
